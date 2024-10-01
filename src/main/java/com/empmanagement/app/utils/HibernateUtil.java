df398622ea40706e5b5ab70e00202026aafb51af
youssefhihi <youssefhihi18@gmail.com>
2024-10-01 21:54:26 +0100

    connect project with db

---
+++ src/main/java/com/empmanagement/app/utils/HibernateUtil.java
@@ -0,0 +1,19 @@
+package com.empmanagement.app.utils;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateUtil {
+    private static final SessionFactory sessionFactory;
+
+    static {
+        try {
+            sessionFactory = new Configuration().configure("hibernate.cfg.xml").buildSessionFactory();
+        } catch (Throwable ex) {
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+}
